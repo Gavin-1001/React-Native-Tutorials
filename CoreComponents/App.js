9873bb6911f8ee4ab9469b3aa77e6c8ee53a41d7
Gavin Shelley <gavinshelley10@gmail.com>
2023-09-08 11:47:14 +0100

    Learned about View Component

---
+++ CoreComponents/App.js
@@ -0,0 +1,13 @@
+import {View} from "react-native"
+
+export default function App(){
+  return( 
+  <View style={{backgroundColor:"plum", flex: 1}}>
+    <View style={{width: 200, height: 200, backgroundColor: "lightblue" }}>
+    </View>
+    
+    <View style={{width: 200, height: 200, backgroundColor: "lightgreen" }}>
+    </View>
+  </View>
+  )
+}
