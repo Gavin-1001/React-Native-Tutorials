f0b8c8c8e22a628a033135107cc94ac25f2450db
Gavin Shelley <gavinshelley10@gmail.com>
2023-09-08 14:35:45 +0100

    Core Components. Images

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,9 +1,16 @@
-import {View, Text} from "react-native"
+import {View, Text, Image, ImageBackground} from "react-native"
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App(){
   return( 
   <View style={{backgroundColor:"plum", flex: 1, padding: 60}}>
       <Text><Text style={{color: 'white'}}>Hello </Text>World</Text>
-  </View>
+      {/* <Image source ={logoImg} style={{width: 300, height: 300}}/>
+      //url needed as a key for remote images
+      <Image source={{uri:'https://picsum.photos/300'}} style={{height: 300, width:300}} /> */}
+      <ImageBackground source={logoImg} style={{flex: 1}}>
+        <Text>IMAGE TEXT</Text>
+      </ImageBackground>
+  </View> 
   );
 }
