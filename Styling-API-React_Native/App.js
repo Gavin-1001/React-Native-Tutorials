847cdb76b9743b140880e5aff2b9313a58b160be
Gavin Shelley <gavinshelley10@gmail.com>
2023-09-11 12:41:11 +0100

    Finished Tutorial 23

--- Styling-API-React_Native/App.js
+++ Styling-API-React_Native/App.js
@@ -4,12 +4,12 @@ export default function App(){
   return (
 
     <View style={styles.container}>
-    <View style={[styles.lightblueBg, styles.box]}>
+    <View style={[styles.lightblueBg, styles.box, styles.boxShadow]}>
       <Text>
         Lightblue box
       </Text>
     </View>
-    <View style={[styles.lightgreenBg, styles.box]}>
+    <View style={[styles.lightgreenBg, styles.box, styles.androidShadow]}>
       <Text>Lightgreen box</Text>
     </View>
   </View>
@@ -19,8 +19,8 @@ export default function App(){
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: "plum", padding: 60},
   box: {
-    width: 100,
-    height: 100,
+    width: 250,
+    height: 250,
     //padding: 10,
     paddingHorizontal: 10,
     paddingVertical: 20,
@@ -36,6 +36,18 @@ const styles = StyleSheet.create({
   lightgreenBg: {
     backgroundColor: "lightgreen",
   },
+  boxShadow: {
+    shadowColor: "#333333",
+    shadowOffset: {
+      width: 6,
+      height: 6
+    },
+    shadowOpcaity: 0.6,
+    shadowRadius: 4,
+  },
+  androidShadow: {
+    elevation: 10,
+  }
 
 
 
