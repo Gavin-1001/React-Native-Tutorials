3df1715771e34a5ab0509bc3c3478c3eb8f64b71
Gavin Shelley <gavinshelley10@gmail.com>
2023-09-11 12:12:28 +0100

    Finished Multiple Styles Tutorial

--- Styling-API-React_Native/App.js
+++ Styling-API-React_Native/App.js
@@ -1,16 +1,36 @@
-
+import {View, Text, StyleSheet} from "react-native"
 
 export default function App(){
   return (
 
     <View style={styles.container}>
-      <Text>Stylesheet API</Text>
-
-    
+    <View style={[styles.lightblueBg, styles.box]}>
+      <Text>
+        Lightblue box
+      </Text>
     </View>
-  )
+    <View style={[styles.lightgreenBg, styles.box]}>
+      <Text>Lightgreen box</Text>
+    </View>
+  </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: "plum", padding: 60},
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
+  },
+
+
+
 });
