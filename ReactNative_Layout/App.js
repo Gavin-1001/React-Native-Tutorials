f36a5287a82cc0228e155a1ec22f6d1239451525
Gavin Shelley <gavinshelley10@gmail.com>
2023-09-11 15:59:10 +0100

    Finished Flex Direction

--- ReactNative_Layout/App.js
+++ ReactNative_Layout/App.js
@@ -4,13 +4,13 @@ import { StyleSheet, View, Text } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{backgroundColor: "#8e9b00", flex: 1}}>Box 1</Box>
+      <Box style={{backgroundColor: "#8e9b00"}}>Box 1</Box>
       <Box style={{backgroundColor: "#b65d1f"}}>Box 2</Box>
       <Box style={{backgroundColor: "#1c4c56"}}>Box 3</Box>
-      <Box style={{backgroundColor: "#ab9156"}}>Box 4</Box>
+      {/* <Box style={{backgroundColor: "#ab9156"}}>Box 4</Box>
       <Box style={{backgroundColor: "#6b0803"}}>Box 5</Box>
       <Box style={{backgroundColor: "#1c4c56"}}>Box 6</Box>
-      <Box style={{backgroundColor: "#b95f21"}}>Box 7</Box>
+      <Box style={{backgroundColor: "#b95f21"}}>Box 7</Box> */}
     </View>
 
     // <View style={{backgroundColor: "plum", flex :1}}></View>
@@ -19,6 +19,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "row-reverse",
     marginTop: 64,
     borderWidth: 6,
     flex: 1,
