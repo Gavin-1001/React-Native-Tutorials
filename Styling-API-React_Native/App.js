cf5e9e13ccebb907a0f5c5e61e624705f80ce6fb
Gavin Shelley <gavinshelley10@gmail.com>
2023-09-11 12:28:44 +0100

    Finished Box Model

--- Styling-API-React_Native/App.js
+++ Styling-API-React_Native/App.js
@@ -21,7 +21,13 @@ const styles = StyleSheet.create({
   box: {
     width: 100,
     height: 100,
-    padding: 10,
+    //padding: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical:10,
+    borderWidth: 2,
+    borderColor: "purple",
+    borderRadius: 5,
   },
   lightblueBg: {
     backgroundColor: "lightblue",
